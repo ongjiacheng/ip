a81789f2df5b323b7189a2fd459c402ed2f4d00a
Ong Jia Cheng <ongjiacheng.2002@gmail.com>
2026-01-21 17:12:02 +0800

    Completed Level 0. Rename, Greet, Exit

---
+++ src/main/java/Tel.java
@@ -0,0 +1,13 @@
+public class Tel {
+    public static void main(String[] args) {
+        String message = """
+                ____________________________________________________________
+                 Hello! I'm [YOUR CHATBOT NAME]
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(message);
+    }
+}
