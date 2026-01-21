5ecda9af5513c8553547553cf5e07a7c35b8fa9a
Ong Jia Cheng <ongjiacheng.2002@gmail.com>
2026-01-21 17:47:30 +0800

    Complete Level 2. Add, List

--- src/main/java/Tel.java
+++ src/main/java/Tel.java
@@ -1,31 +1,32 @@
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Tel {
     public static void main(String[] args) {
-        System.out.println(greeting());
+        System.out.println(
+                newLine() + "    Hello! I'm Tel.\n    What can I do for you?\n" + newLine()
+        );
 
         Scanner scanner = new Scanner(System.in);
         String input = "";
+        List<String> list = new ArrayList<>();
         do {
-            if (!Objects.equals(input, "")) {
-                System.out.println(newLine() + "    " + input + "\n" + newLine());
+            if (Objects.equals(input, "list")) {
+                System.out.print(newLine());
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println("    " + i + ". " + list.get(i - 1));
+                }
+                System.out.print(newLine());
+            } else if (!Objects.equals(input, "")) {
+                list.add(input);
+                System.out.println(newLine() + "    added: " + input + "\n" + newLine());
             }
             input = scanner.nextLine();
 
         } while(!Objects.equals(input, "bye"));
-        System.out.println(farewell());
+        System.out.println(newLine() + "    Bye. Hope to see you again soon!\n" + newLine());
     }
 
     public static String newLine() {
         return "    ____________________________________________________________\n";
-    }
-
-    public static String greeting() {
-        return newLine() + "    Hello! I'm Tel.\n    What can I do for you?\n" + newLine();
-    }
-
-    public static String farewell() {
-        return newLine() + "    Bye. Hope to see you again soon!\n" + newLine();
     }
 }
