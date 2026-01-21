372d1cc423996f77f7d4c04c8e90641fb5b29030
Ong Jia Cheng <ongjiacheng.2002@gmail.com>
2026-01-21 17:35:19 +0800

    Completed Level 1. Echo

--- src/main/java/Tel.java
+++ src/main/java/Tel.java
@@ -1,13 +1,31 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Tel {
     public static void main(String[] args) {
-        String message = """
-                ____________________________________________________________
-                 Hello! I'm Tel.
-                 What can I do for you?
-                ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                ____________________________________________________________
-                """;
-        System.out.println(message);
+        System.out.println(greeting());
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        do {
+            if (!Objects.equals(input, "")) {
+                System.out.println(newLine() + "    " + input + "\n" + newLine());
+            }
+            input = scanner.nextLine();
+
+        } while(!Objects.equals(input, "bye"));
+        System.out.println(farewell());
+    }
+
+    public static String newLine() {
+        return "    ____________________________________________________________\n";
+    }
+
+    public static String greeting() {
+        return newLine() + "    Hello! I'm Tel.\n    What can I do for you?\n" + newLine();
+    }
+
+    public static String farewell() {
+        return newLine() + "    Bye. Hope to see you again soon!\n" + newLine();
     }
 }
