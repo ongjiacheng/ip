46fed402f955f0d03abb1364d32b72a648f2835c
Ong Jia Cheng <ongjiacheng.2002@gmail.com>
2026-01-21 22:15:47 +0800

    Complete Level 3. Mark as Done

--- src/main/java/Tel.java
+++ src/main/java/Tel.java
@@ -9,8 +9,25 @@ public class Tel {
         Scanner scanner = new Scanner(System.in);
         String input = "";
         List<String> list = new ArrayList<>();
+        List<Character> done = new ArrayList<>();
         do {
-            if (Objects.equals(input, "list")) {
+            if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5));
+                done.set(index - 1, 'X');
+                System.out.println(newLine() + "    Nice! I've marked this task as done:");
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println("    " + i + ". [" + done.get(i - 1) + "] " + list.get(i - 1));
+                }
+                System.out.print(newLine());
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7));
+                done.set(index - 1, ' ');
+                System.out.println(newLine() + "    OK, I've marked this task as not done yet:");
+                for (int i = 1; i <= list.size(); i++) {
+                    System.out.println("    " + i + ". [" + done.get(i - 1) + "] " + list.get(i - 1));
+                }
+                System.out.print(newLine());
+            } else if (Objects.equals(input, "list")) {
                 System.out.print(newLine());
                 for (int i = 1; i <= list.size(); i++) {
                     System.out.println("    " + i + ". " + list.get(i - 1));
@@ -18,6 +35,7 @@ public class Tel {
                 System.out.print(newLine());
             } else if (!Objects.equals(input, "")) {
                 list.add(input);
+                done.add(' ');
                 System.out.println(newLine() + "    added: " + input + "\n" + newLine());
             }
             input = scanner.nextLine();
