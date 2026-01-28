bae3549f9fa2eaff566fba27a81e608282265d08
Ong Jia Cheng <ongjiacheng.2002@gmail.com>
2026-01-28 20:37:25 +0800

    Completed Level 8. Dates and Times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,10 @@
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
